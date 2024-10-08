3279ce80d6d15a6f878f7057c945a117350b578b
Emiliano Rotta <emilianorottaf@gmail.com>
2024-10-08 20:48:42 -0300

    Ejercicios

---
+++ empresa_db.sql
@@ -0,0 +1,47 @@
+-- Ejercicio 1
+-- Crea una base de datos llamada escuela_db.
+-- Crea una tabla llamada alumnos con los siguientes campos:
+-- id de tipo entero (int), que será la clave primaria.
+-- nombre de tipo texto (varchar(30)), que no sea nulo.
+-- curso de tipo texto (varchar(30)), que no sea nulo.
+-- Inserta un dato en la tabla alumnos.
+-- Muestra el dato que has insertado.
+-- Inserta 4 datos más en la tabla alumnos.
+-- Muestra 2 elementos de esos datos.
+-- Elimina un dato de la tabla alumnos.
+-- Muestra todos los datos restantes.
+-- Elimina la tabla alumnos.
+-- Elimina la base de datos escuela_db.
+
+
+
+-- Ejercicio 2
+-- Crea una base de datos llamada tienda_db.
+-- Crea una tabla llamada productos con los siguientes campos:
+-- id de tipo entero (int), que será la clave primaria.
+-- nombre de tipo texto (varchar(40)), que no sea nulo.
+-- precio de tipo entero (int), que no sea nulo.
+-- Inserta un dato en la tabla productos.
+-- Muestra el dato que has insertado.
+-- Inserta 4 datos más en la tabla productos.
+-- Muestra 2 elementos de esos datos.
+-- Elimina un dato de la tabla productos.
+-- Muestra todos los datos restantes.
+-- Elimina la tabla productos.
+-- Elimina la base de datos tienda_db.
+
+-- Ejercicio 3
+-- Crea una base de datos llamada biblioteca db.
+-- Crea una tabla llamada libros con los siguientes campos:
+-- id de tipo entero (int), que será la clave primaria.
+-- titulo de tipo texto (varchar(50)), que no sea nulo.
+-- autor de tipo texto (varchar(50)), que no sea nulo.
+-- Inserta un dato en la tabla libros.
+-- Muestra el dato que has insertado.
+-- Inserta 4 datos más en la tabla libros.
+-- Muestra 2 elementos de esos datos.
+-- Elimina un dato de la tabla libros.
+-- Muestra todos los datos restantes.
+-- Elimina la tabla libros.
+-- Elimina la base de datos biblioteca_db.
+
